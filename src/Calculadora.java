7659663926b1f1f095c931da63b8c6debd0ff132
lucasnogueira150 <lucas_nogueira150@estudante.sesisenai.org.br>
2025-08-27 16:02:05 -0300

    Correção de Sintaxe

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -6,13 +6,9 @@ public class Calculadora {
     public static int subtracao(int a, int b) {
         return a - b;
     }
-    
+
     public static void main(String[] args) {
         System.out.println("Soma: " + Calculadora.soma(10, 5));
-        System.out.println("Subtração: " + Calculadora.subtracao(10, 5)
-    );
+        System.out.println("Subtração: " + Calculadora.subtracao(10, 5));
     }
 }
-
-
-   
