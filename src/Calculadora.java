d12c2ce582b5da31abe3bb8439c320880fd17962
lucasnogueira150 <lucas_nogueira150@estudante.sesisenai.org.br>
2025-08-27 15:58:31 -0300

    Adição da soma e subtração

---
+++ src/Calculadora.java
@@ -0,0 +1,18 @@
+public class Calculadora {
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Soma: " + Calculadora.soma(10, 5));
+        System.out.println("Subtração: " + Calculadora.subtracao(10, 5)
+    );
+    }
+}
+
+
+   
